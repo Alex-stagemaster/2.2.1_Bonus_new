1842d5044049c7cd68e2c3f393e6783eda34aca2
Alex-stagemaster <78262386+Alex-stagemaster@users.noreply.github.com>
2021-03-24 14:11:01 +0500

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        float costTicket = 27890.50f; // стоимость билета в ₽
-        int coef = 20;
-        int bonusMiles = (int) (costTicket / coef); // расчёт бонусных миль (1 миля за каждые 20 рублей)
-        String numberAsString = String.format ("%.2f", costTicket);
-        System.out.println("За покупку билета стоимостью "+ (numberAsString) + " ₽, вам было начислено " + bonusMiles + " бонусных мили ");
+        BonusMilesService service = new BonusMilesService();
+        int costTicket = 27890;
+        int bonusMiles = service.calculate(costTicket);
+        System.out.println("За покупку билета стоимостью "+ (costTicket) + " ₽, вам было начислено " + bonusMiles + " бонусных мили ");
     }
 }
 
